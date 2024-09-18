e91e6d0c09e9dd5df4bb407cda9490b463f4e56b
Fuadseid <fuaddbus@gmail.com>
2024-09-19 00:49:55 +0300

    update something

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -32,72 +32,74 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                  Container(
-                    width: 200, // Set the width to the desired size
-                    height: 200, // Set the height to the same value as width
-                    decoration: const BoxDecoration(
-                      shape: BoxShape
-                          .circle, // This ensures the container itself is circular
-                    ),
-                    child: ClipOval(
-                      child: Image.asset(
-                        'lib/asset/logo.jpg',
-                        fit: BoxFit
-                            .cover, // Ensures the image covers the entire circular area
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                    Container(
+                      width: 200, // Set the width to the desired size
+                      height: 200, // Set the height to the same value as width
+                      decoration: const BoxDecoration(
+                        shape: BoxShape
+                            .circle, // This ensures the container itself is circular
+                      ),
+                      child: ClipOval(
+                        child: Image.asset(
+                          'lib/asset/logo.jpg',
+                          fit: BoxFit
+                              .cover, // Ensures the image covers the entire circular area
+                        ),
                       ),
                     ),
+                  const SizedBox(
+                    height: 50,
                   ),
-                const SizedBox(
-                  height: 50,
-                ),
-                const Text(
-                  "Welcome back you have been missed",
-                  style: TextStyle(fontSize: 16),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                MyTextField(
-                    controller: emailController,
-                    hintText: 'Email',
-                    obscureText: false),
-                const SizedBox(
-                  height: 10,
-                ),
-                MyTextField(
-                    controller: passwordController,
-                    hintText: 'Password',
-                    obscureText: true),
-                const SizedBox(
-                  height: 25,
-                ),
-                MyButton(onTap: signIn, text: "Sign In "),
-                const SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Not a member?"),
-                    const SizedBox(
-                      width: 4,
-                    ),
-                    GestureDetector(
-                      onTap: widget.ontap,
-                      child: const Text(
-                        'Register now',
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                  const Text(
+                    "Welcome back you have been missed",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  MyTextField(
+                      controller: emailController,
+                      hintText: 'Email',
+                      obscureText: false),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  MyTextField(
+                      controller: passwordController,
+                      hintText: 'Password',
+                      obscureText: true),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  MyButton(onTap: signIn, text: "Sign In "),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Not a member?"),
+                      const SizedBox(
+                        width: 4,
                       ),
-                    )
-                  ],
-                )
-              ],
+                      GestureDetector(
+                        onTap: widget.ontap,
+                        child: const Text(
+                          'Register now',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
